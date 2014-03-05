ece386aec60d12a0735219468c124e3655242299
Matt Hansen <matt.hansen@oregonstate.edu>
2014-03-04 21:30:29 -0800

    adding twitter bootstrap gem, doing some styling, and precompiling my assets

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Softball::Application.routes.draw do
   end
   
   resources :players
+  
+  root 'teams#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
